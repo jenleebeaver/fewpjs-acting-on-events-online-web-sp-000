4a919373076bad9f627dacd0e774c570f4dce356
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-10 12:23:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
 
-  if ( left < 400) {
+  if ( left > 0 || left < 400) {
     dodger.style.left = `${left + 1}px`;
   }
   else {
